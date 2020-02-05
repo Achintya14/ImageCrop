e75572a61ab5ddfcaac1704fb0072abe4abbb1e2
Gaurav3170 <gauravcs0062@gmail.com>
2020-02-05 17:27:43 +0530

    cameraIssue

--- js/index.js
+++ js/index.js
@@ -1,83 +1,82 @@
-var useFrontCamera = true;
-var track = null;
+var handleOnLoadWindow = function () {
+    var useFrontCamera = true;
+    var track = null;
+    
+    //defining constants
+    const  cameraView = document.querySelector("#camera-view"),
+           cameraOutput = document.querySelector("#camera-output"),
+           cameraSensor = document.querySelector("#camera-sensor"),
+           captureTrigger = document.querySelector("#camera-trigger"),
+           flipTrigger = document.querySelector("#flip-trigger"),
+           saveTrigger = document.querySelector("#save");
+    
+    //access the device camera and steam to cameraView
+    var cameraStart = function () {
+        alert("Trying to detect camera");
+        var foundCamera = false;
+        navigator.mediaDevices.enumerateDevices().then(function(deviceInfo){
+            deviceInfo.forEach(function(device){
+                if ((!foundCamera) && (((device.label.indexOf("front") > -1) && useFrontCamera) || ((device.label.indexOf("back") > -1) && !useFrontCamera))){
+                    
+                    var constraints = { 
+                        video: { deviceID: device.deviceId },
+                        audio: false
+                    };
+                    navigator.mediaDevices.getUserMedia(constraints).then(function(stream){
+                        console.log(stream.getTracks())
+                        track = stream.getTracks()[0];
+                        cameraView.srcObject = stream;
+                        useFrontCamera = !useFrontCamera;
+                    }).catch(function(error){
+                        console.error("Oops. Something is broken.",error);
+                    });
+                    foundCamera = true;
+                    alert("camera found");
+                }
+                if (!foundCamera) {
+                    alert("no camera found - resorting to default");
+                    var constraints = { 
+                        video: {},
+                        audio: false
+                    };
+                    navigator.mediaDevices.getUserMedia(constraints).then(function(stream){
+                        console.log(stream.getTracks())
+                        track = stream.getTracks()[0];
+                        cameraView.srcObject = stream;
+                    }).catch(function(error){
+                        console.error("Oops. Something is broken.",error);
+                    });
+                    foundCamera = true;
+                }
+            });
+        }).catch(function(error){
+            console.error(error);
+        })
+    };
+    
+    captureTrigger.onclick = function(){
+        console.log(cameraView);
+        console.log('capture Trigger clicked');
+        cameraSensor.width = cameraView.videoWidth;
+        cameraSensor.height = cameraView.videoHeight;
+        cameraSensor.getContext("2d").drawImage(cameraView,0,0);
+        cameraOutput.src = cameraSensor.toDataURL("image/png");
+        cameraOutput.classList.add("taken");
+        
+    }
+    
+    saveTrigger.onclick = function(){
+        console.log('save Trigger clicked.');
+        localStorage.setItem("imgCanvas",cameraOutput.src);
+        window.location.href='cropImage.html';
+    }
+    
+    flipTrigger.onclick = function(){
+        alert("Trying to flip camera");
+        cameraStart();
+    }
 
-//defining constants
-const  cameraView = document.querySelector("#camera-view"),
-       cameraOutput = document.querySelector("#camera-output"),
-       cameraSensor = document.querySelector("#camera-sensor"),
-       captureTrigger = document.querySelector("#camera-trigger"),
-       flipTrigger = document.querySelector("#flip-trigger"),
-       saveTrigger = document.querySelector("#save");
-
-//access the device camera and steam to cameraView
-var cameraStart = function () {
-    alert("Trying to detect camera");
-    var foundCamera = false;
-    navigator.mediaDevices.enumerateDevices().then(function(deviceInfo){
-        deviceInfo.forEach(function(device){
-            if ((!foundCamera) && (((device.label.indexOf("front") > -1) && useFrontCamera) || ((device.label.indexOf("back") > -1) && !useFrontCamera))){
-                
-                var constraints = { 
-                    video: { deviceID: device.deviceId },
-                    audio: false
-                };
-                navigator.mediaDevices.getUserMedia(constraints).then(function(stream){
-                    console.log(stream.getTracks())
-                    track = stream.getTracks()[0];
-                    cameraView.srcObject = stream;
-                    useFrontCamera = !useFrontCamera;
-                }).catch(function(error){
-                    console.error("Oops. Something is broken.",error);
-                });
-                foundCamera = true;
-                alert("camera found");
-            }
-            if (!foundCamera) {
-                alert("no camera found - resorting to default");
-                var constraints = { 
-                    video: {},
-                    audio: false
-                };
-                navigator.mediaDevices.getUserMedia(constraints).then(function(stream){
-                    console.log(stream.getTracks())
-                    track = stream.getTracks()[0];
-                    cameraView.srcObject = stream;
-                }).catch(function(error){
-                    console.error("Oops. Something is broken.",error);
-                });
-                foundCamera = true;
-            }
-        });
-    }).catch(function(error){
-        console.error(error);
-    })
+    cameraStart();
 };
 
-captureTrigger.onclick = function(){
-    console.log(cameraView);
-    console.log('capture Trigger clicked');
-    cameraSensor.width = cameraView.videoWidth;
-    cameraSensor.height = cameraView.videoHeight;
-    cameraSensor.getContext("2d").drawImage(cameraView,0,0);
-    cameraOutput.src = cameraSensor.toDataURL("image/png");
-    cameraOutput.classList.add("taken");
-    
-}
-
-saveTrigger.onclick = function(){
-    console.log('save Trigger clicked.');
-    localStorage.setItem("imgCanvas",cameraOutput.src);
-    window.location.href='cropImage.html';
-}
-
-flipTrigger.onclick = function(){
-    currentFacingMode = (currentFacingMode=='user')?'environment':'user';
-    constraints = {
-        video:  { facingMode: currentFacingMode },
-        audio: false
-    }
-    console.log(constraints);
-    cameraStart();
-}
-
-window.addEventListener("load",cameraStart,true);
+window.addEventListener("load",handleOnLoadWindow,true);
