13a615a16f51687600aed0acbeda96e42b3457bc
Gaurav3170 <gauravcs0062@gmail.com>
2020-02-05 13:18:54 +0530

    Made changes in styling of buttons

--- js/cropImage.js
+++ js/cropImage.js
@@ -1,4 +1,6 @@
-const img = document.querySelector("#imageCaptured");
+const img = document.querySelector("#imageCaptured"),
+     submit = document.querySelector("#submit"),
+     verify = document.querySelector("#verify");
 
 var localImg = localStorage.getItem('imgCanvas');
 
@@ -7,5 +9,14 @@ function displayImage() {
     console.log(img);
     img.src = localImg;
 }
+
+submit.onclick = function() {
+    alert('Submit button clicked')
+}
+
+verify.onclick = function() {
+    alert('verify button clicked')
+}
+
 
 window.addEventListener('load',displayImage,true);
