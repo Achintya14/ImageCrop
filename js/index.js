2661677fbfd22d04c142bfb0aaec4b0bf32d0481
Gaurav3170 <gauravcs0062@gmail.com>
2020-02-05 17:14:19 +0530

    flip functionality added

--- js/index.js
+++ js/index.js
@@ -1,10 +1,4 @@
-var currentFacingMode = 'environment';
-var id=0;
-var constraints = { 
-                    video: { facingMode:currentFacingMode },
-                    audio: false
-                };
-
+var useFrontCamera = true;
 var track = null;
 
 //defining constants
@@ -15,32 +9,49 @@ const  cameraView = document.querySelector("#camera-view"),
        flipTrigger = document.querySelector("#flip-trigger"),
        saveTrigger = document.querySelector("#save");
 
-
 //access the device camera and steam to cameraView
-function cameraStart() {
+var cameraStart = function () {
     console.log('camera Stream Started');
-    console.log(constraints);
-    //displaying camera devices
+
+    var foundCamera = false;
     navigator.mediaDevices.enumerateDevices().then(function(deviceInfo){
         deviceInfo.forEach(function(device){
-            alert(device.label+": "+" id = "+device.deviceId)
+            alert(device.label+": "+" id = "+device.deviceId);
+
+            if ((!foundCamera) && (((device.label.indexOf("front") > -1) && useFrontCamera) || ((device.label.indexOf("back") > -1) && !useFrontCamera))){
+                var constraints = { 
+                    video: { deviceID: device.deviceId },
+                    audio: false
+                };
+                navigator.mediaDevices.getUserMedia(constraints).then(function(stream){
+                    console.log(stream.getTracks())
+                    track = stream.getTracks()[0];
+                    cameraView.srcObject = stream;
+                    useFrontCamera = !useFrontCamera;
+                }).catch(function(error){
+                    console.error("Oops. Something is broken.",error);
+                });
+                foundCamera = true;
+            }
+            if (!foundCamera) {
+                var constraints = { 
+                    video: {},
+                    audio: false
+                };
+                navigator.mediaDevices.getUserMedia(constraints).then(function(stream){
+                    console.log(stream.getTracks())
+                    track = stream.getTracks()[0];
+                    cameraView.srcObject = stream;
+                }).catch(function(error){
+                    console.error("Oops. Something is broken.",error);
+                });
+                foundCamera = true;
+            }
         });
     }).catch(function(error){
         console.error(error);
     })
-
-
-    
-   
-    //streaming camera 
-    navigator.mediaDevices.getUserMedia(constraints).then(function(stream){
-        console.log(stream.getTracks())
-        track = stream.getTracks()[0];
-        cameraView.srcObject = stream;
-    }).catch(function(error){
-        console.error("Oops. Something is broken.",error);
-    });
-}
+};
 
 captureTrigger.onclick = function(){
     console.log(cameraView);
